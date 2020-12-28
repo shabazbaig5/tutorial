bdb751f4f88afb93e498f7560f6e02f8e4dbcd2e
Ethan Hunt <shabazbaig5@gmail.com>
2020-12-28 20:43:11 +0530

    adding new files

--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ const Tutorial = () => {
 
     return (
       <div>
-        <p>Hey there from the react application.</p>
+        <p>Hey there from the react application and is everything alright.</p>
+        <Greet />
       </div>
     )
 
 }
 
+
+const Greet = () => {
+  return(
+    <div>This is from another functional component.</div>
+  )
+}
 
 ReactDom.render(<Tutorial />, document.getElementById('root'));
---
+++ src/testing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const testing = () => {
+
+    return <div>Hey there from another, component and something is wrong with my live server</div>
+    
+}
+
+
+export default testing;
