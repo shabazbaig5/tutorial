d238846d77fcda53310ec530efd63c8f224ca0ca
Ethan Hunt <shabazbaig5@gmail.com>
2021-01-04 23:14:10 +0530

    adding changes to the components

--- src/index.js
+++ src/index.js
@@ -1,22 +1,53 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './index.css';
 
-const Tutorial = () => {
+
+
+const bookList = [
+  {
+    bookTitle : "This is just a random book",
+    img : "https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_UY218_.jpg"
+  },
+  {
+    bookTitle : "This is just a random book 2",
+    img : "https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_UY218_.jpg"
+  },
+  {
+    bookTitle : "This is just a random book 3",
+    img : "https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_UY218_.jpg"
+  }
+]
+const List = () => {
 
     return (
-      <div>
-        <p>Hey there from the react application and is everything alright.</p>
-        <Greet />
-      </div>
+      <section className="books-grid">
+       
+        {bookList.map((book, index) => {
+          const {img, bookTitle} = book;
+          console.log(img);
+          return <Book {...book} key={index}/>
+        })
+      }
+
+      </section>
     )
 
 }
 
+const Book = ({img, bookTitle}) => {
 
-const Greet = () => {
-  return(
-    <div>This is from another functional component.</div>
+  // console.log(props);
+  // const {img, bookTitle} = props.book;
+  console.log(bookTitle);
+  return (
+    <article className='book'>
+      <img src={img} alt="book image" />
+      <h4>{bookTitle}</h4>
+      
+    </article>
   )
+
 }
 
-ReactDom.render(<Tutorial />, document.getElementById('root'));
+ReactDom.render(<List />, document.getElementById('root'));
